9bffd4eaf2269a665a52e130137cfe000a0dc4d4
Spencer Ferris <sferris@box.com>
2014-06-26 21:47:50 -0700

    hello, world

--- output.js
+++ output.js
@@ -1,54 +1,25 @@
 'use strict';
 
-	// var e = $.Event('keypress');
- //    e.which = 65; //A
- //    $('#test').val(String.fromCharCode(e.which));
-
-var input_string = "Hello, World!";
-
-print_input(input_string);
+$(document).ready(function(){
 
 
-function print_input(input_string)
-{
-	console.log("hello");
-	for (var i = 0; i < input_string.length; ++i)
+	var input_string = "Hello, World!";
+
+	print_input(input_string);
+
+
+	function print_input(input_string)
 	{
-		// console.log(character);
-		// simulateKeyPress(character);
-		console.log(input_string[i]);
-		simulateKeyPress(input_string[i]);
+		console.log("hello");
+		for (var i = 0; i < input_string.length; ++i)
+		{
+			simulateKeyPress(input_string[i]);
+		}
 	}
-}
 
-function simulateKeyPress(character)
-{
-	// console.log("hello");
-	// jQuery.event.trigger({ type : 'keypress', which : character.charCodeAt(0) });
-	// $(document).sendkeys(character);
-	var e = jQuery.Event("keydown");
-	e.keyCode = character.charCodeAt(0);
-	$('#test').trigger(e);
-}
+	function simulateKeyPress(character)
+	{
+		$('#innerdocbody').children().last().append(character).show();
+	}
 
-// $("#example").click(function() {    
-//     $("#search").focus();
-//     var e = jQuery.Event("keydown");
-//     e.keyCode = 50;                     
-//     $("#search").trigger(e);                    
-// });
-
-// function simulateKeyPress(character) {
-//   var evt = document.createEvent("KeyboardEvent");
-//   evt.initKeyEvent ("keypress", true, true, window,
-//                     0, 0, 0, 0,
-//                     0, character.charCodeAt(0));
-//   var canceled = !body.dispatchEvent(evt);
-//   if(canceled) {
-//     // A handler called preventDefault
-//     alert("canceled");
-//   } else {
-//     // None of the handlers called preventDefault
-//     alert("not canceled");
-//   }
-// }
+});
