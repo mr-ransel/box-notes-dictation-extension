93df723a7bf4a1af1b4b59fcfb2c3c17c1776d9d
Spencer Ferris <sferris@box.com>
2014-06-26 21:08:18 -0700

    add jquery and test index.html

--- output.js
+++ output.js
@@ -1,11 +1,23 @@
 'use strict';
 
+var input_string = "Hello, World!";
+
+print_input(input_string);
+
 function print_input(input_string)
 {
-
+	console.log("hello");
+	for (var i = 0; i < input_string.length; ++i)
+	{
+		// console.log(character);
+		// simulateKeyPress(character);
+		console.log(input_string[i]);
+		simulateKeyPress(input_string[i]);
+	}
 }
 
 function simulateKeyPress(character)
 {
-  jQuery.event.trigger({ type : 'keypress', which : character.charCodeAt(0) });
+	// console.log("hello");
+	jQuery.event.trigger({ type : 'keypress', which : character.charCodeAt(0) });
 }
